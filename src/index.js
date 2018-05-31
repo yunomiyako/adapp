b28263035cffa2240bd146efbfd6ba29b47bc168
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-06-01 07:52:19 +0900

    必要なライブラリインストール

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//install redux
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+
+//reducerを呼び出す
+import rootReducer from "./reducers"
+
+import "./css/index.css"
+import App from "./components/App"
+import registerServiceWorker from "./registerServiceWorker"
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+	<Provider store = {store}>
+		<App />
+	</Provider> ,
+	document.getElementById("root")
+)
+registerServiceWorker()
---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+// Reducerを結合します
+import {combineReducers} from "redux"
+
+export default combineReducers({
+})
+
+//  Example
+/*
+import { combineReducers } from 'redux'
+import todos from './todos'
+import visibilityFilter from './visibilityFilter'
+
+export default combineReducers({
+  todos,
+  visibilityFilter
+})
+*/
