9ba30d9d4013c168cd57ab6f00256abe204a661c
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-06-10 19:40:37 +0900

    ignoreした

--- gitPush.sh
+++ gitPush.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 echo -n "コメント : "
 read COMMENT
-echo -n "プッシュ先 : "
-read name
-name=${name:-master}
-
 git add .
 git commit -m "$COMMENT"
 security delete-internet-password -l  git-codecommit.ap-northeast-1.amazonaws.com
-git push origin $name
+git push origin master
