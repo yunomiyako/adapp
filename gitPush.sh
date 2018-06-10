822a326704c426d1886d998604b47f608269047f
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-06-10 19:30:00 +0900

    テスト

--- gitPush.sh
+++ gitPush.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
+echo -n "コメント : "
 read COMMENT
+echo -n "プッシュ先 : "
+read name
+name=${name:-master}
+
 git add .
 git commit -m "$COMMENT"
 security delete-internet-password -l  git-codecommit.ap-northeast-1.amazonaws.com
--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import "./App.css"
 import TopPage from "./TopPage/TopPage"
 import Header from "./Header/Header"
 
-import { Route, BrowserRouter } from 'react-router-dom';
-import { C_Welcome, C_To, C_React } from './welcometoreact';
+import { Route, BrowserRouter } from "react-router-dom"
+import { C_Welcome, C_To, C_React } from "./welcometoreact"
 
 class App extends Component {
 	render() {
@@ -15,8 +15,8 @@ class App extends Component {
 					<Route path="/" component={Header}/>
 					<Route path="/" component={TopPage}/>
 					<Route path="/welcome" component={C_Welcome}/>
-					<Route path="/to" component={C_To}/>
-					<Route path="/react" component={C_React}/>
+					<Route path="/welcome/to" component={C_To}/>
+					<Route path="/welcome/to/react" component={C_React}/>
 				</div>
 			</BrowserRouter>
 		)
