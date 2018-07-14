572c61d8f71c646f7bb2bec4c2046f56d13efaed
萬年司 <tmsmac@mannenshinoMacBook-Pro.local>
2018-07-14 14:43:24 +0900

    test

--- src/components/TopPage/TopPage.js
+++ src/components/TopPage/TopPage.js
@@ -1,15 +1,45 @@
 import React , {Component}  from "react"
 import { Button } from "semantic-ui-react"
+import { Advertisement } from 'semantic-ui-react'
 import "./TopPage.css"
 
 class TopPage extends Component {
 	render() {
 		return (
 			<div className="TopPage">
-				<Button className="TopPage-logo">最初に設置された記念すべきボタン</Button>
+
+				
+				<div class="ui animated button" tabindex="0">
+  					<div class="visible content">Next</div>
+  					<div class="hidden content">
+    				<i class="right arrow icon"></i>
+  					</div>
+				</div>
+				<div class="ui vertical animated button" tabindex="0">
+  					<div class="hidden content">Shop</div>
+  					<div class="visible content">
+    					<i class="shop icon"></i>
+  					</div>
+				</div>
+
+				<div class="ui labeled button" tabindex="0">
+					<div class="ui button">
+						<i class="heart icon"></i> Like
+					</div>
+					<a class="ui basic label">
+						0
+					</a>
+				</div>
+				<button class="ui twitter button">
+					<i class="twitter icon"></i>
+					Twitter
+				</button>
 			</div>
+			
 		)
 	}
 }
+
+
 
 export default TopPage
