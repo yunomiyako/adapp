ba2d5366dd7c3b16f21a1b5313d99fdfc2cbf790
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-08-17 21:39:14 +0900

    pull

--- src/components/TestPage/TestPage.js
+++ src/components/TestPage/TestPage.js
@@ -1,81 +1,28 @@
 import React , {Component}  from "react"
 import "./TestPage.css"
-import TypingComponent from "./TypingComponent"
+import AdImageUploader from "../AdCreatePage_new/AdImageUploader"
 
 import {Api} from "../../api/Api"
-import { Button , Form , Image , List } from "semantic-ui-react"
+import { } from "semantic-ui-react"
 
 
 class TestPage extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			array : [] ,
-			text : ""
 		}
 	}
 
-	onClickButton() {
-		Api.fetchDynamoTest().then((array) => {
-			console.log(array)
-			return this.setState({array:array})})
+	onChangePictures(pic){
+		console.log(pic)
 	}
-
-	renderArray() {
-		return this.state.array.map((d) => {
-			return (
-				<List.Item key={d.id}>
-					<Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
-					<List.Content >
-						<List.Header>{d.username}</List.Header>
-					</List.Content>
-				</List.Item>)
-		})
-	}
-
-	renderList() {
-		return (<List selection verticalAlign='middle'>
-			{this.renderArray()}
-		</List>)
-	}
-
-	onChangeText(text) {
-		this.setState({
-			text
-		})
-	}
-
-	onClickSubmit() {
-		Api.postDynamoTest(this.state.text).then(() => this.onClickButton())
-
-	}
-
-	onClicktest() {
-		Api.testtm()
-		//console.log(Api.testtm())
-	}
-
-
-
 	render() {
 		return (
 			<div className="TestPage">
-				<div className="TestPage-icon-wrapper">
-					<div/>
-					<TypingComponent/>
-					<Button onClick={() => this.onClickButton()}>test</Button>
-
-					<Form>
-						<Form.Field>
-							<label>User Name</label>
-							<input placeholder='User Name' onChange={(e) => this.onChangeText(e.target.value)}/>
-						</Form.Field>
-						<Button onClick={()=>this.onClickSubmit()}>Submit</Button>
-						<Button onClick={()=>this.onClicktest()}>ttttttt</Button>
-					</Form>
-
-					{this.renderList()}
-				</div>
+				<AdImageUploader
+					id="testpage"
+					onChangePictures={(pic) => this.onChangePictures(pic)}
+				/>
 			</div>
 		)
 	}
