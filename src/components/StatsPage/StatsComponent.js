152f646b43e896b970b2eda6f24ab52c3ba8ef3b
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-09-07 23:22:23 +0900

    topにあれこれ

--- src/components/StatsPage/StatsComponent.js
+++ src/components/StatsPage/StatsComponent.js
@@ -7,29 +7,29 @@ import items from "./test.js"
 class StatsComponent extends Component {
 	renderStats(item, title, desc, classname){
 		return (
-				<div className={classname}>
-					<div className ={style.StatsFrameTitle}>
-						{title}
-					</div>
-					<div className ={style.StatsFrameNumber}>
-						{item.number}
-					</div>
-					<div className ={style.StatsFrameDesc}>
-						{desc}
-					</div>
+			<div className={classname}>
+				<div className ={style.StatsFrameTitle}>
+					{title}
 				</div>
+				<div className ={style.StatsFrameNumber}>
+					{item.number}
+				</div>
+				<div className ={style.StatsFrameDesc}>
+					{desc}
+				</div>
+			</div>
 		)
 	}
 
 	render() {
 		return (
 			<div>
-			<div className={style.aaaa}><h2>何かの詳細</h2></div>
-			<div className={style.StatsComponent}>
-				{this.renderStats(items.items[0], "宣伝された回数", "説明", style.StatsFrame1)}
-				{this.renderStats(items.items[1], "平均評価", "説明", style.StatsFrame2)}
-				{this.renderStats(items.items[0], "何か", "説明", style.StatsFrame3)}
-			</div>
+				<div className={style.aaaa}><h2>何かの詳細</h2></div>
+				<div className={style.StatsComponent}>
+					{this.renderStats(items.items[0], "宣伝された回数", "説明", style.StatsFrame1)}
+					{this.renderStats(items.items[1], "平均評価", "説明", style.StatsFrame2)}
+					{this.renderStats(items.items[0], "何か", "説明", style.StatsFrame3)}
+				</div>
 			</div>
 		)
 	}
