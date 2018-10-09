7f9442d6f316ea8cb0fb40149a4327601f17d446
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-10-09 20:15:25 +0900

    animateOnce={true}

--- src/components/TopPage/toppage_second_comp.js
+++ src/components/TopPage/toppage_second_comp.js
@@ -9,28 +9,30 @@ import "animate.css/animate.min.css"
 import ScrollAnimation from "react-animate-on-scroll"
 
 class Toppage_second extends Component {
-
-
 	render() {
 		return (
 			<div className={style.TopPage_second_wrapper}>
 				<div className={style.TopPageTopPage_second}>
 						
 					<h3 className={style.text1}>
-						<ScrollAnimation animateIn="fadeInRight">
+						<ScrollAnimation animateIn="fadeInRight"
+							animateOnce={true}>
 							{Funcs.getTexts().sectext1}
 						</ScrollAnimation>
 					</h3>
 					<h3 className={style.text2}>
-						<ScrollAnimation animateIn="fadeInRight">
+						<ScrollAnimation animateIn="fadeInRight"
+							animateOnce={true}>
 							<p>・Twitterを使った<font color="#CD5C5C">SNSアドバタイジングサービス</font></p>
 							<br></br>
 						</ScrollAnimation>
-						<ScrollAnimation animateIn="fadeInRight">
+						<ScrollAnimation animateIn="fadeInRight"
+							animateOnce={true}>
 							<p>・プラットフォームにお金を払って広めてもらう従来型の広告とは<font color="#CD5C5C">異なった宣伝方法を提供</font></p>
 							<br></br>
 						</ScrollAnimation>
 						<ScrollAnimation animateIn="fadeInRight">
+						animateOnce={true}
 							<p>・宣伝してくれる人全員にお返しを上げることで<font color="#CD5C5C">SNSに広めてもらう</font>タイプの宣伝</p> 
 						</ScrollAnimation>
 					</h3>
@@ -39,19 +41,22 @@ class Toppage_second extends Component {
 						<div className={style.icons}>
 						
 							<div className={style.icon}>
-								<ScrollAnimation animateIn="fadeInUp">
+								<ScrollAnimation animateIn="fadeInUp"
+									animateOnce={true}>
 									<i className="massive edit icon" ></i>
 									<p className = {style.icon_text}>{Funcs.getTexts().step1}</p>	
 								</ScrollAnimation>				
 							</div>
 							<div className={style.icon2}>
-								<ScrollAnimation animateIn="fadeInUp">
+								<ScrollAnimation animateIn="fadeInUp"
+									animateOnce={true}>
 									<i className="massive gift icon"></i>
 									<p className = {style.icon_text}>{Funcs.getTexts().step2}</p>
 								</ScrollAnimation>
 							</div>
 							<div className={style.icon3}>
-								<ScrollAnimation animateIn="fadeInUp">
+								<ScrollAnimation animateIn="fadeInUp"
+									animateOnce={true}>
 									<i className="massive twitter icon"></i>
 									<p className = {style.icon_text}>{Funcs.getTexts().step3}</p>
 								</ScrollAnimation>
@@ -64,19 +69,22 @@ class Toppage_second extends Component {
 						<div className={style.icons}>
 						
 							<div className={style.icon}>
-								<ScrollAnimation animateIn="fadeInUp">
+								<ScrollAnimation animateIn="fadeInUp"
+									animateOnce={true}>
 									<i className="massive edit icon" ></i>
 									<p className = {style.icon_text} >{Funcs.getTexts().step1}</p>	
 								</ScrollAnimation>				
 							</div>
 							<div className={style.icon2}>
-								<ScrollAnimation animateIn="fadeInUp">
+								<ScrollAnimation animateIn="fadeInUp"
+									animateOnce={true}>
 									<i className="massive gift icon"></i>
 									<p className = {style.icon_text}>{Funcs.getTexts().step2}</p>
 								</ScrollAnimation>
 							</div>
 							<div className={style.icon3}>
-								<ScrollAnimation animateIn="fadeInUp">
+								<ScrollAnimation animateIn="fadeInUp"
+									animateOnce={true}>
 									<i className="massive twitter icon"></i>
 									<p className = {style.icon_text}>{Funcs.getTexts().step3}</p>
 								</ScrollAnimation>
@@ -90,19 +98,22 @@ class Toppage_second extends Component {
 						
 						
 						<div className={style.picon}>
-							<ScrollAnimation animateIn="fadeInUp">
+							<ScrollAnimation animateIn="fadeInUp"
+								animateOnce={true}>
 								<i className="huge edit icon" ></i>
 								<p className = {style.icon_text}>{Funcs.getTexts().step1}</p>	
 							</ScrollAnimation>				
 						</div>
 						<div className={style.picon2}>
-							<ScrollAnimation animateIn="fadeInUp">
+							<ScrollAnimation animateIn="fadeInUp"
+								animateOnce={true}>
 								<i className="huge gift outline icon"></i>
 								<p className = {style.icon_text}>{Funcs.getTexts().step2}</p>
 							</ScrollAnimation>
 						</div>
 						<div className={style.picon3}>
-							<ScrollAnimation animateIn="fadeInUp">
+							<ScrollAnimation animateIn="fadeInUp"
+								animateOnce={true}>
 								<i className="huge twitter icon"></i>
 								<p className = {style.icon_text}>{Funcs.getTexts().step3}</p>
 							</ScrollAnimation>
