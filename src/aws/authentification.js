03228eb930cd5f19f9a6d74396904d972281673b
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-10-14 15:22:35 +0900

    error handling変更

--- src/aws/authentification.js
+++ src/aws/authentification.js
@@ -14,30 +14,42 @@ export default function(callback = () => "") {
 		console.log("サインアップ済み")
 		username = saved_username
 		password = saved_password
+
+		return Auth.currentAuthenticatedUser().catch((e) => {
+			console.log(e)
+			console.log("sign in")
+			//現在ログイン中のユーザがいないため、sign inします
+			return Auth.signIn(username, password).catch((e) => {
+				console.log(e)
+				console.log("sign up")
+				//sign inに失敗したため、登録を行います
+				return Auth.signUp({username,password}).then(() => {
+					console.log("sign in")
+					return Auth.signIn(username, password).catch((e) => {
+						console.log(e)
+						console.log("なんでやねん")
+					})
+				}).catch(e => {
+					console.log(e)
+					console.log("同時にsign upした場合")
+				})
+			})
+		}).then(() => callback())
+
 	} else {
 		console.log("サインアップ")
 		set_username(username)
 		set_password(password)
-	}
 
-	return Auth.currentAuthenticatedUser().catch((e) => {
-		console.log(e)
-		console.log("sign in")
-		//現在ログイン中のユーザがいないため、sign inします
-		return Auth.signIn(username, password).catch((e) => {
-			console.log(e)
-			console.log("sign up")
-			//sign inに失敗したため、登録を行います
-			return Auth.signUp({username,password}).then(() => {
-				console.log("sign in")
-				return Auth.signIn(username, password).catch((e) => {
-					console.log(e)
-					console.log("なんでやねん")
-				})
-			}).catch(e => {
+		return Auth.signUp({username,password}).then(() => {
+			console.log("sign in")
+			return Auth.signIn(username, password).catch((e) => {
 				console.log(e)
-				console.log("同時にsign upした場合")
+				console.log("なんでやねん")
 			})
-		})
-	}).then(() => callback())
+		}).catch(e => {
+			console.log(e)
+			console.log("同時にsign upした場合")
+		}).then(() => callback())
+	}
 }
