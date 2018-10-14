194ad8fb08bed149b00f02e6cce8bcec187cf589
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-10-14 16:14:50 +0900

    ok

--- src/components/StatsPage/URLComponent.js
+++ src/components/StatsPage/URLComponent.js
@@ -5,6 +5,17 @@ import { Link } from "react-router-dom"
 import getBaseUrl from "../../Utils/getBaseUrl"
 
 class StatsComponent extends Component {
+	onClickCopy(event) {
+		event.preventDefault()
+		const elem = document.getElementById("url_field")
+
+		// コピー対象のテキストを選択する
+		elem.select()
+		// 選択しているテキストをクリップボードにコピーする
+		document.execCommand("Copy")
+
+	}
+
 	render() {
 		const baseUrl = getBaseUrl()
 		const link = "/ad_page/" + this.props.id_user + "/" + this.props.id_ad
@@ -17,7 +28,8 @@ class StatsComponent extends Component {
 				</h3>
 				<form class="ui form">
 					<div class="field">
-						<input type="text" value={baseUrl + query}/>
+						<input type="text" id="url_field" value={baseUrl + query}/>
+						<button onClick={(e) => this.onClickCopy(e)}>コピー</button>
 					</div>
 				</form>				
 			</div>
