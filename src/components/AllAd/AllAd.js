02416493f2bba64668c7cd5612973d711701574d
KitaharaMugiro <ngthornym@yahoo.co.jp>
2018-10-24 18:23:04 +0900

    allad改良

--- src/components/AllAd/AllAd.js
+++ src/components/AllAd/AllAd.js
@@ -10,20 +10,8 @@ class AllAd extends Component {
 
 	constructor(props) {
 		super(props)
-
-		
-
 		this.state = {	
-
-			
-			tweet:[],
-			retweet:[],
-			follow:[],
-			fav:[],
-			lookMe:[]
-			,
-			
-			
+			ad_lists : {},
 			focused_ad_list : []
 		}
 
@@ -31,16 +19,14 @@ class AllAd extends Component {
 	}
 	
 	componentDidMount() {
-		
-		this.get_adlist("tweet")
-		
+		const types = AdTypeEnum.getEnums()[0]
+		const first_type = types.string
+		this.get_adlist(first_type)
 	}
 
 
 	async get_adlist(adType) {
-		
 		const ad_list = await fetchAdListForAllAd(adType)
-
 		const headImages = ad_list.map(ad => {
 			if(ad.adObject) {
 				if(ad.adObject.images) {
@@ -54,137 +40,149 @@ class AllAd extends Component {
 			ad.url = urls[index]
 			return ad
 		})
+
+		const ad_lists = this.state.ad_lists
+		ad_lists[adType] = ad_listWithImage
+		this.setState(
+			{
+				ad_lists : ad_lists , 
+				focused_ad_list : ad_listWithImage
+			}
+		)
 		
-		if (adType == "tweet"){
-			this.setState(
-				{
-					tweet: ad_listWithImage,
-					focused_ad_list: ad_listWithImage
+		// if (adType == "tweet"){
+		// 	this.setState(
+		// 		{
+		// 			tweet: ad_listWithImage,
+		// 			focused_ad_list: ad_listWithImage
 					
-				}
+		// 		}
 	
-			)
+		// 	)
 
-		}else if(adType == "retweet"){
+		// }else if(adType == "retweet"){
 
-			this.setState(
-				{
-					retweet: ad_listWithImage,
-					focused_ad_list: ad_listWithImage		
-				}
+		// 	this.setState(
+		// 		{
+		// 			retweet: ad_listWithImage,
+		// 			focused_ad_list: ad_listWithImage		
+		// 		}
 	
-			)
-		}else if(adType == "follow"){
+		// 	)
+		// }else if(adType == "follow"){
 
-			this.setState(
-				{
-					follow: ad_listWithImage,
-					focused_ad_list: ad_listWithImage		
-				}
+		// 	this.setState(
+		// 		{
+		// 			follow: ad_listWithImage,
+		// 			focused_ad_list: ad_listWithImage		
+		// 		}
 	
-			)
-		}else if(adType == "fav"){
+		// 	)
+		// }else if(adType == "fav"){
 
-			this.setState(
-				{
-					fav: ad_listWithImage,
-					focused_ad_list: ad_listWithImage		
-				}
+		// 	this.setState(
+		// 		{
+		// 			fav: ad_listWithImage,
+		// 			focused_ad_list: ad_listWithImage		
+		// 		}
 	
-			)
-		}else if(adType == "lookMe"){
+		// 	)
+		// }else if(adType == "lookMe"){
 
-			this.setState(
-				{
-					looKMe: ad_listWithImage,
-					focused_ad_list: ad_listWithImage		
-				}
+		// 	this.setState(
+		// 		{
+		// 			looKMe: ad_listWithImage,
+		// 			focused_ad_list: ad_listWithImage		
+		// 		}
 	
-			)
-		}
+		// 	)
+		// }
         
 	}
 
 	onChangeTab(tabNum){
-		
+
+		const type = AdTypeEnum.getEnums()[tabNum]
+		const key = type.string
+		const ad_list = this.state.ad_lists[key]
+		if(ad_list){
+			this.setState({
+				focused_ad_list : ad_list
+			})
+		} else {
+			this.get_adlist(key)
+		}
+
 
 		//console.log(tabNum)
-		if (tabNum == 0){
+		// if (tabNum == 0){
+		// 	if(this.state.tweet.length > 0){
+		// 		this.setState(
+		// 			{
+		// 				focused_ad_list: this.state.tweet			
+		// 			})
+		// 	}else{
+		// 		this.get_adlist("tweet")
+		// 	}
+		// }else if(tabNum == 1){
+		// 	if(this.state.retweet.length > 0){
+		// 		this.setState(
+		// 			{
+		// 				focused_ad_list: this.state.retweet
+		// 			})
+		// 	}else{
+		// 		this.get_adlist("retweet")
+		// 	}
+		// }else if(tabNum == 2){
 
-			if(this.state.tweet.length > 0){
-				this.setState(
-					{
-						focused_ad_list: this.state.tweet			
-					})
-			}else{
-				this.get_adlist("tweet")
-			}
-		}else if(tabNum == 1){
-
-			if(this.state.retweet.length > 0){
-
-				
-
-				this.setState(
-					{
-						focused_ad_list: this.state.retweet
-							
-					})
-			}else{
-				this.get_adlist("retweet")
-			}
-		}else if(tabNum == 2){
-
-			if(this.state.follow.length > 0){
+		// 	if(this.state.follow.length > 0){
 
 				
-				this.setState(
-					{
-						focused_ad_list: this.state.follow
+		// 		this.setState(
+		// 			{
+		// 				focused_ad_list: this.state.follow
 							
-					})
-			}else{
-				this.get_adlist("follow")
-			}
-		}else if(tabNum == 3){
+		// 			})
+		// 	}else{
+		// 		this.get_adlist("follow")
+		// 	}
+		// }else if(tabNum == 3){
 
-			if(this.state.fav.length > 0){
-				this.setState(
-					{
-						focused_ad_list: this.state.fav
+		// 	if(this.state.fav.length > 0){
+		// 		this.setState(
+		// 			{
+		// 				focused_ad_list: this.state.fav
 							
-					})
-			}else{
-				this.get_adlist("fav")
-			}
-		}else if(tabNum == 4){
+		// 			})
+		// 	}else{
+		// 		this.get_adlist("fav")
+		// 	}
+		// }else if(tabNum == 4){
 
-			if(this.state.lookMe.length > 0){
-				this.setState(
-					{
-						focused_ad_list: this.state.lookMe
+		// 	if(this.state.lookMe.length > 0){
+		// 		this.setState(
+		// 			{
+		// 				focused_ad_list: this.state.lookMe
 							
-					})
-			}else{
-				this.get_adlist("lookMe")
-			}
-		}
+		// 			})
+		// 	}else{
+		// 		this.get_adlist("lookMe")
+		// 	}
+		// }
 
 
 	}
 
 	render() {
-
-		
+		const types = AdTypeEnum.getEnums()
+		const columns = types.map(type => type.short_title)
 		return (
 			<div className={style.AllAd}>
 
 				<TabComponent 
-				
+					columns= {columns}
 					onChangeTab = {(tabNum) => this.onChangeTab(tabNum)}
-
 					focused_ad_list = {this.state.focused_ad_list}
-
 				/>
 
 			</div>
