b56a4e37156c2d9fdbad966d996e1cd992f24bbc
walk_to_work <fallere15423@gmail.com>
2018-12-18 22:49:05 +0900

    当選率を表示

---
+++ src/components/CampaignPage/Test.js
@@ -0,0 +1,25 @@
+import React , {Component}  from "react"
+
+//css
+import style from "./CampaignPage.css"
+//semantic UI
+import { List , Image} from "semantic-ui-react"
+import { ApplicationAutoScaling } from "aws-sdk/clients/all"
+
+console.log("テスト");
+
+class Test extends Component {
+	render() {
+		return (
+			<div>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<p>テストです</p>
+			</div>
+		)
+	}
+}
+
+export default Test
--- src/components/CampaignPage/WinnerListContainer.js
+++ src/components/CampaignPage/WinnerListContainer.js
@@ -12,7 +12,6 @@ class WinnerListContaienr extends Component {
 	renderWinnerLists(campaign , applicants) {
 		const objects = []
 		if(!campaign) return 
-		console.log(campaign)
 
 		for(const key in campaign.campaign) {
 			const winners = applicants.filter(applicant => {
@@ -20,29 +19,38 @@ class WinnerListContaienr extends Component {
 			})
 			const prize = campaign.campaign[key]
 			const headerLabel = prize.title + " (" + prize.people + "人)"
+			let prob = Number(prize.probability).toFixed(3)*100;
+			prob = prob.toFixed(1)
 			const object = {
-				"applicants" : winners , 
-				"headerLabel" : headerLabel
+				"applicants" : winners ,
+				"headerLabel" : headerLabel,
+				"prob" : prob
 			}
 
 			objects.push(object)
 		}
-        
+
+		console.log(objects)
 		return objects.map(obj => {
 			return (
-				<div key={obj.headerLabel}>
-					<WinnerList
-						headerLabel={obj.headerLabel}
-						applicants={obj.applicants}
-					/>
+				<div key={obj.headerLabel} className={style.prize}>
+					<div className={style.prize_name}>{obj.headerLabel}</div>
+					<div className={style.probability}>この賞品の当選確率は......<span className={style.emphasise}>{obj.prob}%!!</span></div>
+
+					<div className={style.remaing}></div>
+					<div className={style.winnerList}>
+						<WinnerList applicants={obj.applicants} />
+					</div>
 				</div>)
 		})
 	}
-    
+
 	render() {
 		return (
 			<div>
-				{this.renderWinnerLists(this.props.campaign , this.props.applicants)}
+				<ul>
+					{this.renderWinnerLists(this.props.campaign , this.props.applicants)}
+				</ul>
 			</div>
 		)
 	}
