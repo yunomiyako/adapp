3f451fcdb03bf4218a1d7eb52e9278d63e199c71
walk_to_work <fallere15423@gmail.com>
2018-12-23 19:35:28 +0900

    外れの人を表示するようにした

--- src/components/CampaignPage/WinnerListContainer.js
+++ src/components/CampaignPage/WinnerListContainer.js
@@ -44,12 +44,15 @@ class WinnerListContaienr extends Component {
 	renderWinnerLists(campaign , applicants) {
 		const objects = []
 		if(!campaign) return 
+		console.log(campaign);
 
 		for(const key in campaign.campaign) {
 			const winners = applicants.filter(applicant => {
 				return applicant.result == key
 			})
 			const prize = campaign.campaign[key]
+			console.log(prize);
+			
 			const headerLabel = prize.title
 			let prob = Number(prize.probability).toFixed(3)*100
 			prob = prob.toFixed(1)
@@ -70,22 +73,45 @@ class WinnerListContaienr extends Component {
 		return objects.map(obj => {
 			return (
 				<div key={obj.headerLabel} className={style.prize}>
-					<div className={style.prize_name}><span>{obj.headerLabel}</span></div>
-					<div className={style.probability}>この賞品の当選確率は......<span className={style.emphasise}>{obj.prob}%!!</span></div>
-					{this.ItemStatus(obj.item_num , obj.remain_num)}
-					<div className={style.winnerList}>
-						<WinnerList applicants={obj.applicants} />
-					</div>
+				<div className={style.prize_name}><span>{obj.headerLabel}</span></div>
+				<div className={style.probability}>この賞品の当選確率は<span className={style.emphasise}>{obj.prob}%!!</span></div>
+				{this.ItemStatus(obj.item_num , obj.remain_num)}
+				<div className={style.winnerListTitle}>当選者</div>
+				<div className={style.winnerList}>
+				<WinnerList applicants={obj.applicants} />
+				</div>
 				</div>)
 		})
+	}
+
+	renderMissList( campaign , applicants){
+		const winners = applicants.filter(applicant => {
+			return applicant.result == "外れ"
+		})
+		const headerLabel = "外れ"
+		const object = {
+			"applicants" : winners ,
+			"headerLabel" : headerLabel,
+		}
+
+		if(winners.length > 0){
+			return(
+				<div key={object.headerLabel} className={style.prize}>
+					<div className={style.prize_name}><span>外れ</span></div>
+					<div className={style.winnerList}>
+					<WinnerList applicants={object.applicants} /></div>
+				</div>
+			)
+		}
 	}
 
 	render() {
 		return (
 			<div>
-				<ul>
-					{this.renderWinnerLists(this.props.campaign , this.props.applicants)}
-				</ul>
+			<ul>
+			{this.renderWinnerLists(this.props.campaign , this.props.applicants)}
+			{this.renderMissList(this.props.campaign , this.props.applicants)}
+			</ul>
 			</div>
 		)
 	}
